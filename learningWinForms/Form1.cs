3e4d4c914380f887876355fc0143000ed401931a
Antongo22 <80984465+Antongo22@users.noreply.github.com>
2023-10-18 12:41:20 +0300

    вирус

--- learningWinForms/Form1.cs
+++ learningWinForms/Form1.cs
@@ -17,7 +17,7 @@ namespace learningWinForms
         public Form1()
         {
             InitializeComponent();
-            timer1.Interval = 300;
+            timer1.Interval = 1;
             timer1.Tick += Act;
             timer1.Start();
         }
@@ -29,14 +29,17 @@ namespace learningWinForms
             form.BackColor = Color.FromArgb(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256));
             form.Location = new Point(rnd.Next(0, Screen.PrimaryScreen.Bounds.Width - form.Width), 
                              rnd.Next(0, Screen.PrimaryScreen.Bounds.Height - form.Height));
-            form.Size = new Size(rnd.Next(100, 400), rnd.Next(100, 400));
+            form.Size = new Size(rnd.Next(10, 1000), rnd.Next(00, 1000));
             form.Show();
         }
 
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            BackColor = Color.FromArgb(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256));
+            Location = new Point(rnd.Next(0, Screen.PrimaryScreen.Bounds.Width - Width),
+                             rnd.Next(0, Screen.PrimaryScreen.Bounds.Height - Height));
+            Size = new Size(rnd.Next(10, 1000), rnd.Next(10, 1000));
         }
     }
 }
