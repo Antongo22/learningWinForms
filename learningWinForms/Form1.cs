7a020ff83547faa6df389c0ad3d82583865016b8
Antongo22 <80984465+Antongo22@users.noreply.github.com>
2023-10-18 11:57:25 +0300

    Добавьте файлы проекта.

---
+++ learningWinForms/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace learningWinForms
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        Timer timer1 = new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 300;
+            timer1.Tick += Act;
+            timer1.Start();
+        }
+
+        private void Act(object sender, EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256));
+            Form2 form = new Form2();
+            form.BackColor = Color.FromArgb(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256));
+            form.Location = new Point(rnd.Next(0, Screen.PrimaryScreen.Bounds.Width - form.Width), 
+                             rnd.Next(0, Screen.PrimaryScreen.Bounds.Height - form.Height));
+            form.Size = new Size(rnd.Next(100, 400), rnd.Next(100, 400));
+            form.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ learningWinForms/Form2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace learningWinForms
+{
+    public partial class Form2 : Form
+    {
+        Random rnd = new Random();
+        Timer timer1 = new Timer();
+
+        public Form2()
+        {
+            InitializeComponent();
+            timer1.Interval = 500;
+            timer1.Tick += Act;
+            timer1.Start();
+        }
+
+        private void Act(object sender, EventArgs e)
+        {
+            this.Location = new Point(rnd.Next(0, Screen.PrimaryScreen.Bounds.Width - this.Width),
+                 rnd.Next(0, Screen.PrimaryScreen.Bounds.Height - this.Height));
+            this.BackColor = Color.FromArgb(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256));
+            this.Size = new Size(rnd.Next(100, 400), rnd.Next(100, 400));
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            this.Location = new Point(rnd.Next(0, Screen.PrimaryScreen.Bounds.Width - this.Width),
+                 rnd.Next(0, Screen.PrimaryScreen.Bounds.Height - this.Height));
+        }
+    }
+}
