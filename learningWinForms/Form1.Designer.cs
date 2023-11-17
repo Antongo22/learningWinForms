a14021101d30909f2fce08e00eccd899ed3e2223
Anton <80984465+Antongo22@users.noreply.github.com>
2023-11-17 13:41:02 +0300

    кт20

--- learningWinForms/Form1.Designer.cs
+++ learningWinForms/Form1.Designer.cs
@@ -28,117 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // pictureBox1
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(12, 12);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 0;
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(199, 12);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(130, 147);
-            this.listBox1.TabIndex = 1;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(12, 39);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 2;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(118, 38);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 20);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Добавить";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(118, 65);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 21);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Удалить";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(12, 65);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(75, 20);
-            this.textBox2.TabIndex = 5;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 142);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(43, 13);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Итого: ";
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(12, 91);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(121, 22);
-            this.button3.TabIndex = 7;
-            this.button3.Text = "Добавить покупку";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(691, 589);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(332, 164);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.comboBox1);
+            this.ClientSize = new System.Drawing.Size(715, 613);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Чек";
+            this.Text = "Распорядок дня";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- learningWinForms/Form1.cs
+++ learningWinForms/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,61 +13,103 @@ namespace learningWinForms
 {
     public partial class Form1 : Form
     {
-        Dictionary<string, int> products = new Dictionary<string, int>();
-        
+        Timer timer;
+        int min = 0;
+        int hou = 0;
+        string time = "";
+
+        string h = "00";
+        string m = "00";
 
         public Form1()
         {
+          
             InitializeComponent(); 
+        }
+
+        void UpdateTime()
+        {
+            time = "Время - " + h + ":" + m;
+            this.Text = time;
+        }
+
+        void SetPic()
+        {
+            if (hou < 7)
+            {
+                pictureBox1.BackgroundImage = Image.FromFile("00-00.png");
+            }
+            else if (hou < 10)
+            {
+                pictureBox1.BackgroundImage = Image.FromFile("07-00.png");
+            }
+            else if (hou < 15)
+            {
+                pictureBox1.BackgroundImage = Image.FromFile("10-00.png");
+            }
+            else if (hou < 18)
+            {
+                pictureBox1.BackgroundImage = Image.FromFile("15-00.png");
+            }
+            else if (hou < 20)
+            {
+                pictureBox1.BackgroundImage = Image.FromFile("18-00.png");
+            }
+            else
+            {
+                pictureBox1.BackgroundImage = Image.FromFile("20-00.png");
+            }
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+            timer = new Timer();
+            timer.Interval = 1;
+            timer.Tick += Act;
+            timer.Start();
         }
 
-        void Culc()
+        private void Act(object sender, EventArgs e)
         {
-            int res = 0;
-
-            foreach(var item in products)
+            if (min < 60)
             {
-                res += item.Value;
+                if (min < 10)
+                {
+                    m = "0" + min.ToString();
+                    min++;
+                }
+                else
+                {
+                   m = min.ToString();
+                    min++;
+                }
+            }
+            else
+            {
+                min = 0;
+                if (hou >= 23)
+                {
+                    hou = 0;
+                    h = "0" + hou.ToString();
+                    return;
+                }
+                if (hou < 9)
+                {
+                    h = hou.ToString();
+                    hou++;
+                    h = "0" + hou.ToString();
+                }
+                else
+                {
+                    hou++;
+                    h = hou.ToString();
+                }
+
             }
 
-            label1.Text = "Итого:" + res;
-        }
-
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            comboBox1.Items.Add(textBox1.Text);
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            
-            string[] prod = comboBox1.Text.Split(' ');
-
-            if (products.ContainsKey(prod[0]) )
-            {
-                return;
-            }
-
-            products.Add(prod[0], int.Parse(prod[1]) * int.Parse(textBox2.Text));
-            listBox1.Items.Add(comboBox1.Text + " x " + textBox2.Text);
-
-            Culc();
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            string[] prod = listBox1.SelectedItem.ToString().Split(' ');
-
-            listBox1.Items.RemoveAt(listBox1.SelectedIndex);
-
-            products.Remove(prod[0]);
-            Culc();
+            SetPic();
+            UpdateTime();
         }
     }
 }
