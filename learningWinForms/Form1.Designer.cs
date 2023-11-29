2faa87fd5fde3a9b6f8f432e3c2756e4e440c58d
Anton <80984465+Antongo22@users.noreply.github.com>
2023-11-29 11:15:27 +0300

    кт23

--- learningWinForms/Form1.Designer.cs
+++ learningWinForms/Form1.Designer.cs
@@ -28,185 +28,192 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.сохранитьКакToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.вставкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.копироватьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.вставитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.вырезатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.шрифтToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.настройкиШрифтаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.фонToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.настройкиФонаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.menuStrip1.SuspendLayout();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(12, 59);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(691, 431);
-            this.textBox1.TabIndex = 0;
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.файлToolStripMenuItem,
-            this.вставкаToolStripMenuItem,
-            this.шрифтToolStripMenuItem,
-            this.фонToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(713, 24);
-            this.menuStrip1.TabIndex = 1;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // файлToolStripMenuItem
-            // 
-            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.открытьToolStripMenuItem,
-            this.сохранитьКакToolStripMenuItem});
-            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
-            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
-            this.файлToolStripMenuItem.Text = "Файл";
-            // 
-            // открытьToolStripMenuItem
-            // 
-            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
-            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.открытьToolStripMenuItem.Text = "Открыть";
-            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
-            // 
-            // сохранитьКакToolStripMenuItem
-            // 
-            this.сохранитьКакToolStripMenuItem.Name = "сохранитьКакToolStripMenuItem";
-            this.сохранитьКакToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.сохранитьКакToolStripMenuItem.Text = "Сохранить как";
-            this.сохранитьКакToolStripMenuItem.Click += new System.EventHandler(this.сохранитьКакToolStripMenuItem_Click);
-            // 
-            // вставкаToolStripMenuItem
-            // 
-            this.вставкаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.копироватьToolStripMenuItem,
-            this.вставитьToolStripMenuItem,
-            this.вырезатьToolStripMenuItem});
-            this.вставкаToolStripMenuItem.Name = "вставкаToolStripMenuItem";
-            this.вставкаToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
-            this.вставкаToolStripMenuItem.Text = "Вставка";
-            // 
-            // копироватьToolStripMenuItem
-            // 
-            this.копироватьToolStripMenuItem.Name = "копироватьToolStripMenuItem";
-            this.копироватьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.копироватьToolStripMenuItem.Text = "Копировать";
-            this.копироватьToolStripMenuItem.Click += new System.EventHandler(this.копироватьToolStripMenuItem_Click);
-            // 
-            // вставитьToolStripMenuItem
-            // 
-            this.вставитьToolStripMenuItem.Name = "вставитьToolStripMenuItem";
-            this.вставитьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.вставитьToolStripMenuItem.Text = "Вставить";
-            this.вставитьToolStripMenuItem.Click += new System.EventHandler(this.вставитьToolStripMenuItem_Click);
-            // 
-            // вырезатьToolStripMenuItem
-            // 
-            this.вырезатьToolStripMenuItem.Name = "вырезатьToolStripMenuItem";
-            this.вырезатьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.вырезатьToolStripMenuItem.Text = "Вырезать";
-            this.вырезатьToolStripMenuItem.Click += new System.EventHandler(this.вырезатьToolStripMenuItem_Click);
-            // 
-            // шрифтToolStripMenuItem
-            // 
-            this.шрифтToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.настройкиШрифтаToolStripMenuItem});
-            this.шрифтToolStripMenuItem.Name = "шрифтToolStripMenuItem";
-            this.шрифтToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
-            this.шрифтToolStripMenuItem.Text = "Шрифт";
-            // 
-            // настройкиШрифтаToolStripMenuItem
-            // 
-            this.настройкиШрифтаToolStripMenuItem.Name = "настройкиШрифтаToolStripMenuItem";
-            this.настройкиШрифтаToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
-            this.настройкиШрифтаToolStripMenuItem.Text = "Настройки шрифта";
-            this.настройкиШрифтаToolStripMenuItem.Click += new System.EventHandler(this.настройкиШрифтаToolStripMenuItem_Click);
-            // 
-            // фонToolStripMenuItem
-            // 
-            this.фонToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.настройкиФонаToolStripMenuItem});
-            this.фонToolStripMenuItem.Name = "фонToolStripMenuItem";
-            this.фонToolStripMenuItem.Size = new System.Drawing.Size(42, 20);
-            this.фонToolStripMenuItem.Text = "Фон";
             // 
             // настройкиФонаToolStripMenuItem
             // 
             this.настройкиФонаToolStripMenuItem.Name = "настройкиФонаToolStripMenuItem";
-            this.настройкиФонаToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.настройкиФонаToolStripMenuItem.Text = "Настройки фона";
-            this.настройкиФонаToolStripMenuItem.Click += new System.EventHandler(this.настройкиФонаToolStripMenuItem_Click);
+            this.настройкиФонаToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
             // 
-            // textBox2
+            // pictureBox1
             // 
-            this.textBox2.Location = new System.Drawing.Point(12, 33);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(207, 20);
-            this.textBox2.TabIndex = 2;
+            this.pictureBox1.Location = new System.Drawing.Point(16, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(452, 478);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(626, 27);
+            this.button1.Location = new System.Drawing.Point(474, 269);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Добавить";
+            this.button1.Size = new System.Drawing.Size(227, 106);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Нарисовать";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(475, 387);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(225, 102);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox4);
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(475, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(225, 112);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "График";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton3);
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.Location = new System.Drawing.Point(474, 130);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(225, 114);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Цвет";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 19);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(56, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Синус";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(6, 42);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(68, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Косинус";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(7, 66);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(63, 17);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Эллипс";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(8, 88);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(88, 17);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "Окружность";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(11, 20);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(70, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Красный";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(11, 43);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(56, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Синий";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(11, 66);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(67, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Жёлтый";
+            this.radioButton3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(713, 502);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Редактор";
+            this.Text = "График";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem сохранитьКакToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem вставкаToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem копироватьToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem вставитьToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem вырезатьToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem шрифтToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem настройкиШрифтаToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem фонToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem настройкиФонаToolStripMenuItem;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
     }
 }
 
--- learningWinForms/Form1.cs
+++ learningWinForms/Form1.cs
@@ -11,6 +11,7 @@ using System.Windows.Forms;
 using static System.Windows.Forms.LinkLabel;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using System.IO;
+using System.Threading;
 
 namespace learningWinForms
 {
@@ -27,112 +28,92 @@ namespace learningWinForms
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
-        }
 
-        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        }
+        private void DrawCoordinateAxes()
         {
-            // Открытие диалогового окна для выбора файла
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            Graphics graphics = pictureBox1.CreateGraphics();
+            Pen axisPen = new Pen(Color.Black, 2f);
 
-            if (openFileDialog.ShowDialog() != DialogResult.OK) return;
+            graphics.DrawLine(axisPen, 0, pictureBox1.Height / 2, pictureBox1.Width, pictureBox1.Height / 2);
 
-            // Чтение данных из файла и сохранение в массив
-            string text = File.ReadAllText(openFileDialog.FileName);
+            graphics.DrawLine(axisPen, pictureBox1.Width - 10, pictureBox1.Height / 2 - 5, pictureBox1.Width, pictureBox1.Height / 2);
+            graphics.DrawLine(axisPen, pictureBox1.Width - 10, pictureBox1.Height / 2 + 5, pictureBox1.Width, pictureBox1.Height / 2);
 
-            textBox1.Text = text;
+            graphics.DrawLine(axisPen, pictureBox1.Width / 2, 0, pictureBox1.Width / 2, pictureBox1.Height);
+
+            graphics.DrawLine(axisPen, pictureBox1.Width / 2 - 5, 10, pictureBox1.Width / 2, 0);
+            graphics.DrawLine(axisPen, pictureBox1.Width / 2 + 5, 10, pictureBox1.Width / 2, 0);
         }
 
-        private void сохранитьКакToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-
-            saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
-
-            if (saveFileDialog.ShowDialog() == DialogResult.OK)
-            {
-                // Получаем путь к выбранному файлу
-                string filePath = saveFileDialog.FileName;
-
-                try
-                {
-                    using (StreamWriter writer = new StreamWriter(filePath))
-                    {
-                        writer.Write(textBox1.Text);
-                    }
-
-                    MessageBox.Show("Файл успешно сохранен.", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"Ошибка при сохранении файла: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            }
-        }
-
-        private void копироватьToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            if (!string.IsNullOrEmpty(textBox1.SelectedText))
-            {
-                Clipboard.SetText(textBox1.SelectedText);
-            }
-        }
-
-        private void вставитьToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            string clipboardText = Clipboard.GetText();
-
-            int cursorPosition = textBox1.SelectionStart;
-
-            textBox1.Text = textBox1.Text.Insert(cursorPosition, clipboardText);
-
-            textBox1.SelectionStart = cursorPosition + clipboardText.Length;
-        }
-
-        private void вырезатьToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            // Проверяем, есть ли в textBox1 выделенный текст
-            if (!string.IsNullOrEmpty(textBox1.SelectedText))
-            {
-                // Копируем выделенный текст в буфер обмена
-                Clipboard.SetText(textBox1.SelectedText);
-
-                // Затем удаляем выделенный текст из textBox1
-                textBox1.SelectedText = "";
-            }
-        }
-
-        private void настройкиШрифтаToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            FontDialog fontDialog= new FontDialog();
-            fontDialog.ShowDialog();
-            textBox1.Font= fontDialog.Font;
-        }
-
-        private void настройкиФонаToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            ColorDialog colorDialog = new ColorDialog();
-            colorDialog.ShowDialog();
-            textBox1.BackColor = colorDialog.Color;
-        }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            persons.Add(textBox2.Text);
-            textBox2.Text = "";
-            textBox1.Text = $"Уважаемые {GetPeople()}{Environment.NewLine}Приглашаем вас на всеобщую мобилизвцию!{Environment.NewLine}Явка обязательна.";
+            DrawCoordinateAxes();
+            Graphics graphics = pictureBox1.CreateGraphics();
+
+            Color color = Color.Black;
+            if (radioButton1.Checked)
+            {
+                color = Color.Red;
+            }
+            else if (radioButton2.Checked)
+            {
+                color = Color.Blue;
+            }
+            else if (radioButton3.Checked)
+            {
+                color = Color.Yellow;
+            }
+
+
+            Pen pen = new Pen(color, 3f);
+            Point[] pointsS = new Point[1000];
+            Point[] pointsC = new Point[1000];
+
+
+            for (int i = 0; i < 1000; i++)
+            {
+                if (checkBox1.Checked)
+                {
+                    pointsS[i] = new Point(i, (int)(Math.Sin((double)i / 10 - 20) * 100 + 230));
+                }
+                if (checkBox2.Checked)
+                {
+                    pointsC[i] = new Point(i, (int)(Math.Cos((double)i / 10 - 20) * 100 + 230));
+                }
+            }
+
+            if (checkBox3.Checked)
+            {
+                int centerX = 200;
+                int centerY = 200;
+                int width = 50;
+                int height = 30;
+                Rectangle ellipseRect = new Rectangle(centerX - width / 2, centerY - height / 2, width, height);
+                graphics.DrawEllipse(pen, ellipseRect);
+            }
+            if (checkBox4.Checked)
+            {
+                int centerX = 400;
+                int centerY = 200;
+                int radius = 50;
+                Rectangle circleRect = new Rectangle(centerX - radius, centerY - radius, 2 * radius, 2 * radius);
+                graphics.DrawEllipse(pen, circleRect);
+            }
+
+            graphics.DrawLines(pen, pointsS);
+            graphics.DrawLines(pen, pointsC);
         }
 
-        string GetPeople()
+        private void button2_Click(object sender, EventArgs e)
         {
-            string per = "";
-            foreach (var person in persons)
-            {
-                per += person + ", ";
-            }
-            return per;
+            Graphics graphics = pictureBox1.CreateGraphics();
+            graphics.Clear(pictureBox1.BackColor);
+            DrawCoordinateAxes();
         }
+
+       
     }
 }
     
